c2c87c4e73dbc85dd5fe7105bc35f3c87562a956
Deniz <mdcengiz@gmail.com>
2020-05-13 13:34:45 -0500

    Add GCD

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,33 +9,43 @@
 import UIKit
 
 class ViewController: UITableViewController  {
-
+    
     var pictures = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Storm Viewer"
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-
-        for item in items {
-            if item.hasPrefix("nssl") {
-                pictures.append(item)
-                pictures.sort()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sharedTapped))
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let fm = FileManager.default
+            let path = Bundle.main.resourcePath!
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    self.pictures.append(item)
+                }
+                self.pictures.sort()
                 
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
+            
         }
-      
+        
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
@@ -53,5 +63,13 @@ class ViewController: UITableViewController  {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
+    @objc func sharedTapped(){
+        
+        let message = "Donwload the project"
+        
+        let vc = UIActivityViewController(activityItems: [message], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc , animated: true)    }
+    
 }
 
